453f041aa87434026e48eb616f15a6655eb96426
Kyle Nakamura <kjnakamura@apu.edu>
2018-11-13 15:02:57 -0800

    Initial Commit

---
+++ APU Feed/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  APU Feed
+//
+//  Created by Kyle Nakamura on 11/13/18.
+//  Copyright © 2018 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
